9064786c63c2a4ff1f9fed15e94eede4408d2d80
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2024-09-07 12:21:38 +0900

    fix deploy script to doing maven build

--- FreTreApiCore/conte/scripts/deploy.sh
+++ FreTreApiCore/conte/scripts/deploy.sh
@@ -9,6 +9,7 @@ usage() {
 # デフォルトのWARファイルパスと環境
 WAR_FILE_PATH="../../target/FreTreApiCore.war"
 ENVIRONMENT="local"
+BUILD_TARGET_POM_PATH="../../../FreTreServer/pom.xml"
 
 # 引数をパース
 while getopts "f:e:" opt; do
@@ -32,6 +33,23 @@ esac
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
+# FreTreServerのpomからpackage生成
+echo "Cleaning maven..."
+mvn clean -f $BUILD_TARGET_POM_PATH
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to clean with FreTreServer."
+    exit 1
+fi
+echo "Finish clean maven."
+
+echo "Pachaging maven..."
+mvn package -f $BUILD_TARGET_POM_PATH -DskipTests
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to package with FreTreServer."
+    exit 1
+fi
+echo "Finish package maven."
+
 # WARファイルを指定の場所にコピー
 TARGET_WAR_PATH="../fretre/FreTreApiCore.war"
 echo "Copying WAR file from $WAR_FILE_PATH to $TARGET_WAR_PATH..."
@@ -43,9 +61,17 @@ cd ../
 # Dockerコンテナを停止
 echo "Stopping Docker containers..."
 docker-compose -f "$COMPOSE_FILE" down
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to Deployment on Docker Stopping."
+    exit 1
+fi
 
 # Dockerコンテナを起動
 echo "Starting Docker containers with $COMPOSE_FILE..."
 docker-compose -f "$COMPOSE_FILE" up -d
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to Deployment on Docker Starting."
+    exit 1
+fi
 
 echo "Deployment completed successfully."
