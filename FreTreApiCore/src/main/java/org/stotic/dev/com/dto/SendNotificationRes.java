e01685d3d5a99a6c1b111caebea8f389afe1959e
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2024-10-28 14:31:54 +0900

    複数のdevicetokenを受けた時でもリクエストをAPNSに送れるよう修正

--- FreTreApiCore/src/main/java/org/stotic/dev/com/dto/SendNotificationRes.java
+++ FreTreApiCore/src/main/java/org/stotic/dev/com/dto/SendNotificationRes.java
@@ -3,7 +3,12 @@ package org.stotic.dev.com.dto;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.stotic.dev.com.api.exception.ApiResultCode;
+import org.stotic.dev.com.client.ApiResponse;
 import org.stotic.dev.com.model.PushNotificationApnsResponseData;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -15,10 +20,21 @@ public class SendNotificationRes extends ApiResultDto {
 
     public  SendNotificationRes() {}
 
-    public SendNotificationRes(int resultCode, PushNotificationApnsResponseData responseData) {
-        super(resultCode == SUCCESS_RESULT_CODE ? ApiResultCode.SUCCESS_SEND_NOTIFICATION.getCode() : ApiResultCode.FAILURE_SEND_NOTIFICATION.getCode());
-        if(responseData != null) {
-            errorReason = responseData.getReason();
+    public SendNotificationRes(List<ApiResponse<PushNotificationApnsResponseData>> responseList) {
+        super(
+                responseList.stream()
+                        .map(ApiResponse::getStatus)
+                        .filter(Predicate.not(Predicate.isEqual(SUCCESS_RESULT_CODE)))
+                        .findFirst().isEmpty()
+                        ? ApiResultCode.SUCCESS_SEND_NOTIFICATION.getCode()
+                        : ApiResultCode.FAILURE_SEND_NOTIFICATION.getCode()
+        );
+        List<String> errorReasons = responseList.stream()
+                .filter(reasons -> reasons.getData() != null && !reasons.getData().getReason().isEmpty())
+                .map(data -> data.getData().getReason())
+                .toList();
+        if(!errorReasons.isEmpty()) {
+            errorReason = errorReasons.stream().collect(Collectors.joining(", "));
         }
     }
 
